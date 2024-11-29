e43208d01ed0245c8a105d52147dfcd8aa715bc3
mimisukeMaster <81568941+mimisukeMaster@users.noreply.github.com>
2024-11-29 19:18:33 +0900

    feat: 球を落とす敵の処理
    Unity側の設定はまだしていない

--- Assets/Scripts/DoropAttackEnemy.cs
+++ Assets/Scripts/DoropAttackEnemy.cs
@@ -3,21 +3,38 @@ using UnityEngine;
 public class DoropAttackEnemy : EnemyController 
 {
     
-    public float Altitude =10.0f;
+    public float Altitude = 10.0f;
+    public float DropInterval = 1.0f;
+    public GameObject Bullet; 
+
+    private float nextDropTime;
 
     
-    void Start()
-    {   transform.position = new Vector3(transform.position.x,Altitude,transform.position.z);
-        rb.constraints = RigidbodyConstraints.FreezePositionY;
-        //検知範囲を上書きする、高度の約sqrt(2/√3)倍
-        DetectionRadius = Altitude * 1.15f;
-        
+    protected override void Start()
+    {   
+        // 継承元のStart()を実行
+        base.Start();
 
+        transform.position = new Vector3(transform.position.x, Altitude, transform.position.z);
+        rb.constraints = RigidbodyConstraints.FreezePositionY;
+
+        // 検知範囲を上書きする、高度の約2/√3倍
+        DetectionRadius = Altitude * 1.15f;
     }
 
-
-    void Update()
+    protected override void Update() 
     {
-        
+        // 継承元のUpdate()を実行
+        base.Update();
+
+        if(Time.time > nextDropTime && isChasing)
+        {
+            // 生成と同時に落下していく
+            GameObject bullet = Instantiate(Bullet, transform.position, Quaternion.identity);
+
+            Destroy(bullet, 5.0f);
+
+            nextDropTime = Time.time + DropInterval;
+        }
     }
 }
