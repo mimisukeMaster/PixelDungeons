f3afac09c3f9ca2d64893c05f809c6a1856ec7d6
mimisukeMaster <81568941+mimisukeMaster@users.noreply.github.com>
2024-12-14 00:04:14 +0900

    update: 動きに連動した攻撃処理を武器にも実装

--- Assets/Scripts/NearAttackEventHandler.cs
+++ Assets/Scripts/NearAttackEventHandler.cs
@@ -10,9 +10,14 @@ public class NearAttackEventHandler : MonoBehaviour
         attackController = GetComponentInParent<AttackController>();
         nearAttackEnemy = GetComponentInParent<NearAttackEnemy>();
     }
+
+    /// <summary>
+    /// 近距離敵の攻撃イベント発生時の処理
+    /// </summary>
+    /// <remarks>アニメーションのタイミングでUnity側から呼ばれる</remarks>
     public void AttackEvent()
     {
-        attackController.NearAttack(transform.parent.position, nearAttackEnemy.AttackDistance);
+        attackController.NearAttack(transform.position, nearAttackEnemy.AttackDistance);
 
     }
 }
--- Assets/Scripts/WeaponEventTrigger.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class WeaponEventTrigger : MonoBehaviour
-{
-    
-}
