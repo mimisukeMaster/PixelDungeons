bb5bba518f41324c40c40dff3bf7b2fac7edbc7b
mimisukeMaster <81568941+mimisukeMaster@users.noreply.github.com>
2024-12-19 16:35:33 +0900

    feat: 敵の動きと攻撃タイミングの同期

--- Assets/Scripts/MiddleBossEnemy.cs
+++ Assets/Scripts/MiddleBossEnemy.cs
@@ -5,13 +5,21 @@ public class MiddleBossEnemy : EnemyController
 {
     [Tooltip("攻撃時間間隔")]
     public float SmashInterval = 1.0f;
+    [Tooltip("攻撃の中心")]
+    public Transform SmashArea;
+    [Tooltip("攻撃半径")]
+    public float smashRange;
+
     private Animator animator;
+    private AttackController attackController;
     private float nextSmashTime;
 
     protected override void Start()
     {
         base.Start();
         animator = GetComponentInChildren<Animator>();
+        attackController.Init("Player", Attack);
+
     }
     protected override void Update()
     {
