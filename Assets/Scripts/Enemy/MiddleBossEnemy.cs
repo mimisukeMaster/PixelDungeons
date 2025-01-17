3e2bc5708cb4a70966a1beeefa21917cf5faebf2
Hasuo <hasuoren0726@gmail.com>
2025-01-17 09:10:00 +0900

    refactor:敵についているAttackControllerを消去

--- Assets/Scripts/Enemy/MiddleBossEnemy.cs
+++ Assets/Scripts/Enemy/MiddleBossEnemy.cs
@@ -3,12 +3,10 @@ using UnityEngine;
 public class MiddleBossEnemy : EnemyController
 {
     protected Animator animator;
-    public AttackController attackController;
 
     protected override void Start()
     {
         base.Start();
         animator = GetComponentInChildren<Animator>();
-        attackController.Init("Player", Attack, float.PositiveInfinity,100);
     }
 }
