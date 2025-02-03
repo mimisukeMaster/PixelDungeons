bfa26781c4caab8c575da33a1e635357f61ca810
mimisukeMaster <81568941+mimisukeMaster@users.noreply.github.com>
2025-02-03 16:37:01 +0900

    fix: クリア後同じシーンがロードされるバグの修正

--- Assets/Scripts/Stage/NextStageButton.cs
+++ Assets/Scripts/Stage/NextStageButton.cs
@@ -3,17 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class NextStageButton : MonoBehaviour
 {
-    private int nowSceneIndex;
-
-    private void Start()
-    {
-        nowSceneIndex = SceneManager.GetActiveScene().buildIndex;
-    }
-
     public void OnPress()
     {
         // ステージをロード
-        SceneManager.LoadScene(nowSceneIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         Time.timeScale = 1.0f;
     }
 
