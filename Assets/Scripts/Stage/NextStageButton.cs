8231047850b03f901bed5fd3bfaae53a8d8da0df
mimisukeMaster <81568941+mimisukeMaster@users.noreply.github.com>
2025-02-06 00:03:28 +0900

    fix: クリア後のタイトルボタンを削除
    タイトルシーンに戻った時に生じるバグの対処。シングルトン(Coreオブジェクト)の温存が難しいため。

--- Assets/Scripts/Stage/NextStageButton.cs
+++ Assets/Scripts/Stage/NextStageButton.cs
@@ -9,11 +9,4 @@ public class NextStageButton : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         Time.timeScale = 1.0f;
     }
-
-    public void OnTitle()
-    {
-        // タイトルをロード
-        SceneManager.LoadScene(0);
-        Time.timeScale = 1.0f;
-    }
 }
