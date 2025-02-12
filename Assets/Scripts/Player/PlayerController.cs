52840a35bfb98a228394a9b498c1fc3a9b1b2df2
mimisukeMaster <81568941+mimisukeMaster@users.noreply.github.com>
2025-02-13 07:51:10 +0900

    fix: プレイヤーが動かないバグを修正

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -64,7 +64,6 @@ public class PlayerController : MonoBehaviour
 
         // インスタンス生成
         controls = new ControlActions();
-        Debug.Log("controll actions initialized");
 
         // InputSystemでの入力に対応するリスナーを追加
         controls.Player.Jump.performed += OnJumpPerformed;
