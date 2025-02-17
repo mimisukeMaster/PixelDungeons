54253042cd929fd652ff159df24c37def54217e3
Hasuo <hasuoren0726@gmail.com>
2025-02-18 00:06:20 +0900

    feat:ドラゴンの攻撃

--- Assets/Scripts/Enemy/Dragon.cs
+++ Assets/Scripts/Enemy/Dragon.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Dragon : EnemyController
@@ -9,18 +11,45 @@ public class Dragon : EnemyController
         hover,
         charge
     }
+    [Header("Dragon")]
     public DragonState dragonState = DragonState.init;
     [Tooltip("通常時の高度")]
     public float normalAltitude;
     public float AltitudeRandomness;
     private Transform myTransform;
-    public Transform MoveTargetSphere;//デバッグ用
+    private Transform MoveTargetSphere;//デバッグ用
     public Animator animator;
+
+    [Space(20)]
+    [Header("追尾弾連射")]
+    private Coroutine homingCoroutine;
+    public GameObject homingPrefab;
+    public int homingDamage;
+    public float homingSpeed;
+    public float homingAngle;
+    public float homingFireRate;
+    public int homingNumber;
+    public float homingInterval;
+    public float HomingSpread;
+
+    [Header("本体狙い連射")]
+    private Coroutine shotsCoroutine;
+    public GameObject shotsPrefab;
+    public int shotsDamage;
+    public float shotsSpeed;
+    public float shotsFireRate;
+    public int shotsNumber;
+    public float shotsChargeTime;
+
+    [Header("突進")]
+    public bool isCharging = false;
+    public int chargeDamage;
+
     [Header("Init")]
     public float ascentionSpeed;
     private float moveUpdateCountDown;//次の行動に向かう時間
 
-    [Header("move")]
+    [Header("周回")]
     public float moveSpeed;
     public float moveRange;//移動目標のプレイヤーからの距離の上限
     public float rotationSpeed;
@@ -30,14 +59,16 @@ public class Dragon : EnemyController
     public int MinMoveUpdateTime;
     public int MaxMoveUpdateTime;
 
-    [Header("hover")]
+
+    [Header("ホバー")]
     public Transform WingRootTransform;
     public float HoverBodyRotationSpeed;
     public float HoverProbability;
     public float MinHoverTime;
     public float MaxHoverTime;
 
-    [Header("Charge")]
+
+    [Header("突進")]//突進関連
     public float ChargeProbability;
     public float ChargeSpeed;
     public float ChargeRotation;
@@ -48,7 +79,8 @@ public class Dragon : EnemyController
     }
     private ChargeState chargeState;
 
-    [Header("首")]
+
+    [Header("首の向き")]
     public Transform NeckTransform;
     private Quaternion neckRotationTarget;
     public float NeckRotationSpeed;
@@ -88,9 +120,7 @@ public class Dragon : EnemyController
                 myTransform.position = myTransform.position + Vector3.up * ascentionSpeed * Time.deltaTime;
                 if(myTransform.position.y >= normalAltitude)
                 {
-                    moveUpdateCountDown = Random.Range(MinMoveUpdateTime,MaxMoveUpdateTime);
-                    MoveTargetPosition = targetPlayer.transform.position + new Vector3(Random.Range(-10,10),normalAltitude + Random.Range(-AltitudeRandomness,AltitudeRandomness),Random.Range(-10,10));
-                    dragonState = DragonState.flyAround;
+                    ChangeToFlyAround();
                 }
                 break;
             case DragonState.flyAround:
@@ -125,6 +155,7 @@ public class Dragon : EnemyController
         }
         if(moveUpdateCountDown <= 0)
         {
+            StopCoroutine(homingCoroutine);
             float randomNumber = Random.value;
             if(randomNumber > HoverProbability)
             {
@@ -141,13 +172,30 @@ public class Dragon : EnemyController
         }
     }
 
+    private IEnumerator FlyAroundCoroutine()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(homingInterval);
+            for(int i = 0;i < homingNumber;i++)
+            {
+                Quaternion randomSpread = Quaternion.Euler(Random.Range(-HomingSpread,HomingSpread),Random.Range(-HomingSpread,HomingSpread),Random.Range(-HomingSpread,HomingSpread));
+                GameObject homingBullet = Instantiate(homingPrefab,transform.position,randomSpread * Quaternion.Euler(-90,0,0) * myTransform.rotation);
+                homingBullet.GetComponent<AttackController>().Init("Player",30,homingDamage,homingSpeed,homingAngle,1000);
+                yield return new WaitForSeconds(homingFireRate);
+            }
+        }
+    }
+
     private void ChangeToFlyAround()
     {
         animator.SetBool("Hover",false);
         Vector3 moveTransform = targetPlayer.transform.position + new Vector3(Random.Range(-10,10),normalAltitude + Random.Range(-AltitudeRandomness,AltitudeRandomness),Random.Range(-10,10));
         MoveTargetPosition = moveTransform;
         moveUpdateCountDown = Random.Range(MinMoveUpdateTime,MaxMoveUpdateTime);
+        homingCoroutine = StartCoroutine(FlyAroundCoroutine());
         dragonState = DragonState.flyAround;
+        Debug.Log(dragonState);
     }
 
     private void Hover()
@@ -163,6 +211,7 @@ public class Dragon : EnemyController
 
         if(moveUpdateCountDown <= 0)
         {
+            StopCoroutine(shotsCoroutine);
             float randomNumber = Random.value;
             WingRootTransform.rotation = myTransform.rotation;
             if(randomNumber > ChargeProbability)
@@ -176,12 +225,25 @@ public class Dragon : EnemyController
         }
     }
 
+    private IEnumerator HoverCoroutine()
+    {
+        yield return new WaitForSeconds(shotsChargeTime);
+        for(int i = 0;i < shotsNumber;i++)
+        {
+            GameObject bullet = Instantiate(shotsPrefab,myTransform.position,Quaternion.identity);
+            bullet.GetComponent<AttackController>().Init("Player",shotsDamage,100,1);
+            bullet.GetComponent<Rigidbody>().linearVelocity = (targetPlayer.transform.position-myTransform.position+ new Vector3(0,0.5f,0)).normalized*shotsSpeed;
+            yield return new WaitForSeconds(shotsFireRate);
+        }
+    }
+
     private void ChangeToHover()
     {
-        Debug.Log(dragonState);
+        shotsCoroutine = StartCoroutine(HoverCoroutine());
         animator.SetBool("Hover",true);
         moveUpdateCountDown = Random.Range(MinHoverTime,MaxHoverTime);
         dragonState = DragonState.hover;
+        Debug.Log(dragonState);
     }
 
     private void Charge()
@@ -206,6 +268,7 @@ public class Dragon : EnemyController
                     MoveTargetPosition = chargeTarget;
                     break;
                 case ChargeState.charge:
+                    isCharging = false;
                     ChangeToFlyAround();
                     break;
             }
@@ -214,12 +277,23 @@ public class Dragon : EnemyController
 
     private void ChangeToCharge()
     {
-        Debug.Log(dragonState);
+        isCharging = true;
         animator.SetBool("Hover",false);
         chargeState = ChargeState.dive;
         Vector3 nextMoveTarget = myTransform.position;
         nextMoveTarget.y = 1;
         MoveTargetPosition = nextMoveTarget;
         dragonState = DragonState.charge;
+        Debug.Log(dragonState);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(isCharging);
+        if(isCharging && collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<HPController>().Damaged(chargeDamage,Vector3.zero);
+            Debug.Log("Charge Damage");
+        }
     }
 }
